5358e623e6d0ccf9a026cb27899d4ecaebbac6f4
ZlatkoDamyanov <zlatko.sv.damyanov@gmail.com>
2026-05-27 15:10:41 +0300

    UserService and IUserService implemented

---
+++ Services/Implementations/UserService.cs
@@ -0,0 +1,49 @@
+﻿using Data;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly MovieCatalogDbContext _context;
+
+        public UserService(MovieCatalogDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddUser(string username, string email)
+        {
+            var user = new User
+            {
+                Username = username,
+                Email = email
+            };
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+
+        public List<User> GetAll()
+        {
+            return _context.Users.ToList();
+        }
+
+        public User GetByEmail(string email)
+        {
+            return _context.Users
+                .FirstOrDefault(x => x.Email == email);
+        }
+
+        public User GetById(int id)
+        {
+            return _context.Users.Find(id);
+        }
+    }
+}
+
---
+++ Services/Interfaces/IUserService.cs
@@ -0,0 +1,20 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface IUserService
+    {
+        void AddUser(string username, string email);
+
+        User GetById(int id);
+
+        User GetByEmail(string email);
+
+        List<User> GetAll();
+    }
+}
